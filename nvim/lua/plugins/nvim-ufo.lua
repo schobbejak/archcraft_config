c3bcca2d73cd1d0e391e42aa73dfe7d3d2910a6e
schobbejak <jmvanselm@gmail.com>
2024-03-18 23:16:52 +0100

    Add override for mason and lspconfig

--- nvim/lua/plugins/nvim-ufo.lua
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -1,4 +1,11 @@
 return {
-    'kevinhwang91/nvim-ufo',
-    requires= 'kevinhwang91/promise-async'
+    {
+        'kevinhwang91/nvim-ufo',
+        dependencies = "kevinhwang91/nvim-ufo",
+        event = "VimEnter",
+        init = function()
+            vim.opt.foldlevel = 99
+            vim.opt.foldlevelstart = 99
+        end,
+    }
 }
