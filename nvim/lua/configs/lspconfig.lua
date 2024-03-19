41e969fb7d0520426a33ccc81502652c2f0a87f2
schobbejak <jmvanselm@gmail.com>
2024-03-20 00:50:36 +0100

    Configure lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,20 +4,13 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "python-lsp-server" }
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
-
--- typescript
-lspconfig.tsserver.setup {
+lspconfig["python-lsp-server"].setup({
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
-}
+  filetypes = {"python"}
+})
+
