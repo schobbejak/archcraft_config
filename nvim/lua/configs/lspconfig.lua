cf03a963cde888a2fd927c289cf38284b9e62944
schobbejak <jmvanselm@gmail.com>
2024-03-20 01:10:12 +0100

    Latest changes

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "python-lsp-server" }
+local servers = { "python-lsp-server", "lua-language-server" }
 
 -- lsps with default config
 lspconfig["python-lsp-server"].setup({
@@ -11,5 +11,11 @@ lspconfig["python-lsp-server"].setup({
   on_init = on_init,
   capabilities = capabilities,
   filetypes = {"python"}
+})
+
+lspconfig["lua-language-server"].setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities
 })
 
--- nvim/lua/plugins/mason-custom.lua
+++ nvim/lua/plugins/mason-custom.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       ensure_installed = {
         "python-lsp-server",
+        "lua-language-server",
       }
     }
   }
