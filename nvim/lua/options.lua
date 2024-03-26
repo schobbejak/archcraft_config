da1a13a7f796b355fad680d359a46ba6042b4661
schobbejak <jmvanselm@gmail.com>
2024-03-26 21:56:21 +0100

    Initial folding settings

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,20 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local opt = vim.opt
+
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- opt.foldcolumn = "0"
+-- opt.foldtext = ""
+
+-- Minimum level of fold closed by default
+opt.foldlevel = 99
+
+-- Upon editing a buffer what level of folds should be open by default
+opt.foldlevelstart = 1
+
+-- Limits how deelp code gets folded
+opt.foldnestmax = 4
